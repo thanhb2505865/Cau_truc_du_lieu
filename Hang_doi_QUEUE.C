b91348ddc5d21d0522620e6ac4d5b5d5eb98155f
Tah <thanhb2505865@student.ctu.edu.vn>
2026-06-30 17:28:53 +0700

    uppdate

--- Hang_doi_QUEUE.C
+++ Hang_doi_QUEUE.C
@@ -12,11 +12,43 @@ typedef struct {
 }Queue;
 
 
-void MAKENULL_QUEUE(Queue *Q) {
-    Q->Front = -1;
+void MAKENULL_QUEUE(Queue *Q) { //HAM tao queue rong 
+    Q->Front = -1; // Đây trừ -1 vì khi có 1 ptu đâu tiên vào chỉ cần cho front và rear ++ Xong sao đó cho rear tăng lên là đươc
     Q->Rear = -1;
 }
 
+int EMPTLY_QUEUE(Queue Q) { //Dua lam MAKENULL_QUEUE de bic
+    return Q.Front == -1 & Q.Rear == -1;
+}
+
+int FULL_QUEUE(Queue Q) { //Khi hang doi day => Mang day
+    return Q.Rear + 1  //(Q.Rear - Q.Front + 1) == Maxlength chưa tối ưa
+}
+
+int FRONT(Queue Q) { //Lay phan tu dau tien trong hang doi
+    return Q.Elements[Q.Front];
+}
+
+void ENQUEUE(ElementType X, Queue *Q) {
+    if(FULL_QUEUE(*Q)) {
+        return;
+    }
+    else {
+        Q->Rear++;
+        Q->Elements[Q->Rear] = X;
+    }
+}
+
+void DEQUEUE(Queue *Q) {
+    if(FULL_QUEUE(*Q)) {
+        return;
+    }
+    else {
+        Q->Front++;
+
+    }
+
+}
 int main() {
     return 0;
 }
