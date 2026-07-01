ed699535a7d676059ddb9e000c1db493556f4d43
Tah <thanhb2505865@student.ctu.edu.vn>
2026-07-01 23:03:28 +0700

    Tah

--- Hang_doi_tron_QUEUE.c
+++ Hang_doi_tron_QUEUE.c
@@ -1,3 +1,4 @@
+// Co the noi day la hang được nâng cấp để front và rear = M - 1 mà front > 0
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
@@ -9,7 +10,6 @@ typedef struct {
     ElementType Elements[Maxlength];
     int Front, Rear;
 }Queue;
-
 
 void MAKENULL_QUEUE(Queue *Q) { //HAM tao queue rong 
     Q->Front = -1; // Đây trừ -1 vì khi có 1 ptu đâu tiên vào chỉ cần cho front và rear ++ Xong sao đó cho rear tăng lên là đươc
@@ -28,38 +28,24 @@ int FRONT(Queue Q) { //Lay phan tu dau tien trong hang doi
     return Q.Elements[Q.Front];
 }
 
-void ENQUEUE(ElementType X, Queue *Q) {
-    if(FULL_QUEUE(*Q)) {
+void ENQUEUE (ElementType X, Queue* Q) {
+    if (FULL_QUEUE(*Q)) {
         return;
     }
     else {
-        if(Q->Front > 0 && Q->Rear == Maxlength - 1) {
-            int i;
-            for (i = Q->Front; i <= Q->Rear; i++) {
-                Q->Elements[i - Q->Front] = Q->Elements[Q->Front]; 
-            }
-            Q->Rear = Maxlength - Q->Front;
-            Q->Front = 0;
-        }
+        Q->Rear = (Q->Rear + 1) % Maxlength;
+        Q->Elements[Q->Rear] = X;
     }
-    Q->Rear++;
-    Q->Elements[Q->Rear] = X;
 }
 
-
-
-void DEQUEUE(Queue *Q) {
-    if(FULL_QUEUE(*Q)) {
+void DEQUEUE(Queue *Q) { // Luu y truong hop co 1 phan tu 
+    if(EMPTLY_QUEUE(*Q)) {
         return;
-    }
-    if (Q->Front == Q->Rear || Q->Front > Q->Rear) {// Bat huu
+    };
+    if (Q->Front == Q->Rear) { // Q->Front == 0 sai vi co the Q->Front co the = 0; => chi co 1 truong hop la khi moi khoi dau co 1 phan tu 
         MAKENULL_QUEUE(Q);
     }
     else {
-        Q->Front++;
+        Q->Front =  (Q->Front + 1) % Maxlength;
     }
-}
-
-int main() {
-    return 0;
 }
