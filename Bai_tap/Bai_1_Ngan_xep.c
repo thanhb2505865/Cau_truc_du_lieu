ddd86cded64294609fb76b15316806e089ee5f64
Tah <thanhb2505865@student.ctu.edu.vn>
2026-07-01 14:40:14 +0700

    Tah

--- Bai_tap/Bai_1_Ngan_xep.c
+++ Bai_tap/Bai_1_Ngan_xep.c
@@ -2,28 +2,28 @@
 #include <string.h>
 #include <stdlib.h>
 
-typedef int ElementType;
+typedef char ElementType;
 #define Maxlength 100
 #define M 100
 
 typedef struct {
-    int DuLieu[M];
+    ElementType DuLieu[M];
     int Dinh;
-}NganXep;
+} NganXep;
 
 void khoitao(NganXep *S) {
     S->Dinh = M;
 }
 
-int ktRong(NganXep S) {
+ElementType ktRong(NganXep S) {
     return S.Dinh == M;
 }
 
-int ktDay(NganXep S) {
+ElementType ktDay(NganXep S) {
     return S.Dinh == 0;
 }
 
-void them(int x, NganXep* S) {
+void them(ElementType x, NganXep* S) {
     if (ktDay(*S)) {
         return;
     }
@@ -42,7 +42,7 @@ void xoa(NganXep *S) {
     }
 }
 
-int giatriDinh(NganXep S) {
+ElementType giatriDinh(NganXep S) {
     return S.DuLieu[S.Dinh];
 }
 
@@ -50,14 +50,40 @@ void hienthi(NganXep* S) {
     NganXep *P = (NganXep*)malloc(sizeof(NganXep));
     *P = *S; // Tao ra tro P chua du lieu ss voi NganXep S hoac C2 NganXep P = *S; khi nay sẽ tao ra 1 biến P chưa dữ liệu của con trỏ S 
     while(!ktRong(*P)) {
-        printf("%d", P->DuLieu[P->Dinh]);
+        printf("%c", P->DuLieu[P->Dinh]);
         xoa(P);     // C2 k cần
     }
     free(P);
 }
 
+void doiNhiPhan(int n, NganXep* S) {
+    khoitao(S);
+    while(n > 0) { 
+        them(n % 2, S);
+        n /= 2;
+    }
+}
+// 
+
+void inThaplucphan(int n) {
+    char chu[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
+    NganXep* S = (NganXep*)malloc(sizeof(NganXep));
+    khoitao(S);
+    while(n > 0) { 
+        push(chu[n % 16], S);
+        n /= 16;
+    }
+    while(!ktRong(*S)) {
+        printf("%c", giatriDinh(*S));
+        xoa(S);
+    }
+}
+
 
 
 int main() {
+    int n;
+    scanf("%d",&n);
+    inThaplucphan(n);
     return 0;
 }
--- Ngan_xep_STACK.C
+++ Ngan_xep_STACK.C
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 
 // Thu vien Stack 
-typedef int ElementType;
+typedef char ElementType;
 #define Maxlength 100
 
 typedef struct {
@@ -29,7 +29,7 @@ void PUSH(ElementType X, Stack* S) { //Them phan tu vao Stack
 }
 
 void POP(Stack* S) { //Xoa phan tu dau trong Stack
-    if(EMPTLY_STACK(*S)) { // Sao day lai de la con tro hay gì &
+    if(EMPTLY_STACK(*S)) { // *S dua gia tri ben trong cua con tro 
         return;
     }
     else {
@@ -46,6 +46,21 @@ ElementType TOP(Stack S) { //Lay phan tu dau tien trong Stack
     }
 }
 
+void inThaplucphan(int n) { // Doi gia tri thap luc phan sang luc phan
+    char chu[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'E', 'F'};
+    Stack* S = (Stack*)malloc(sizeof(Stack));
+    MAKENULL_STACK(S);
+    while(n > 0) { 
+        PUSH(chu[n % 16], S);
+        n /= 16;
+    }
+    while(!EMPTLY_STACK(*S)) {
+        TOP(*S);
+        POP(S);
+    }
+}
+
 int main() {
+    inThaplucphan(50);
     return 0;
 }
