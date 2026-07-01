a5a28ecdf270b852fc7e756e01919a84c45f1a75
Tah <thanhb2505865@student.ctu.edu.vn>
2026-07-01 12:51:23 +0700

    Update Bai_1_DanhSachDac.c

--- Bai_tap/Bai_1_DanhSachDac.c
+++ Bai_tap/Bai_1_DanhSachDac.c
@@ -1,9 +1,12 @@
+
+#define Maxlength 100
+
 typedef struct {
     int Elements [Maxlength];
     int Last;
 } List;
 
-int locate1(int n, List L) {
+int locate(int n, List L) {
     int i;
     for (i = L.Last - 1; i >= 0; i--) {
         if(L.Elements[i] == n) return i + 1;
@@ -13,7 +16,7 @@ int locate1(int n, List L) {
 
 void deleteList(int p, List* L) {
     int i;
-    if (locate1(p, *L)) {
+    if (locate(p, *L)) {
         for (i = p - 1; i < L->Last; i++) {
         L->Elements[i] = L->Elements[i + 1];
         L->Last--;
@@ -29,3 +32,37 @@ void erase(int x, List* L) { // Ham xoa ki tu dau tien cua x trong List
     return;
 }
 
+void removeAll(int x, List* L) { 
+    while(locate(x, *L) != L->Last + 1) {
+        deleteList(locate(x, *L), L);
+    }
+}
+
+void insertList(int x, int p, List *L) { //p la vi tri tương đương chỉ số + 1
+    int i;
+    for (i = L->Last; i > p - 1; i--) {
+        L->Elements[i] = L->Elements[i - 1];       
+    }
+    L->Elements[p - 1] = x;
+    L->Last++;
+}
+
+void printList(List L) { // Ham xoa x trong List 
+    int i = 0;
+    while(i < L.Last) {
+        printf("%d ", L.Elements[i]);
+        i++;
+    }
+}
+
+void readList(List *L) {
+    L = (List*)malloc(sizeof(List));
+    scanf("%d", &L->Last);
+    int i = 0;
+    int n;
+    while(i < L->Last) {
+        scanf("%d", &n);
+        insertList(n, i, L);
+        i++;
+    }
+}
